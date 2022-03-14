a0774b1bc3f864e725013f25b7b2625f7adf09c6
Faizall e <faizalle96@gmail.com>
2022-03-14 20:45:53 +0700

    Initial commit

---
+++ routes/api.js
@@ -0,0 +1,26 @@
+// TODO 2: SETUP ROUTING (ROUTER)
+//import controller patiens
+const PatiensController = require("../controllers/PatienstController")
+
+//import express
+const express = require("express")
+
+//membuat object router
+const router = express.Router()
+
+//buat routing home
+router.get("/",(req,res) => {
+    res.send("Semangat UAS Projectnya..")
+})
+
+//Routing untuk pasien
+router.get("/patiens",PatiensController.index)
+router.post("/patiens",PatiensController.store)
+router.put("/patiens/:id",PatiensController.update)
+router.delete("/patiens/:id",PatiensController.destroy)
+router.get("/patiens/:id",PatiensController.show)
+router.get("/patiens/search/:name",PatiensController.search)
+router.get("/patiens/status/:status",PatiensController.searchStatus)
+
+//export module routing
+module.exports = router;
